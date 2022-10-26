2e494d8e009e4cfe75acc9467664b09192534cce
Yaroslav Baienko <zerhug@gmail.com>
2022-10-27 00:45:16 +0300

    roll dice game

--- main.py
+++ main.py
@@ -20,18 +20,28 @@ if __name__ == '__main__':
         statistics3.append(player3[0])
 
     counter1 = Counter(statistics1)
+    counter2 = Counter(statistics2)
+    counter3 = Counter(statistics3)
     results1 = []
+    results2 = []
+    results3 = []
     for key, value in counter1.items():
         results1.append((key, value))
 
+    for key, value in counter2.items():
+        results2.append((key, value))
+
+    for key, value in counter3.items():
+        results3.append((key, value))
+
     testlist2 = [(elem2, elem1) for elem1, elem2 in results1]
-    # testlist3 = [(elem2, elem1[0]) for elem1, elem2 in statistics2]
-    # testlist4 = [(elem2, elem1[0]) for elem1, elem2 in statistics3]
+    testlist3 = [(elem2, elem1) for elem1, elem2 in results2]
+    testlist4 = [(elem2, elem1) for elem1, elem2 in results3]
     plt.figure(figsize=(20, 20))
     plt.subplot(131)
     lines = plt.scatter(*zip(*results1))
-    # plt.subplot(132)
-    # lines2 = plt.scatter(*zip(*testlist3))
-    # plt.subplot(133)
-    # lines3 = plt.scatter(*zip(*testlist4))
+    plt.subplot(132)
+    lines2 = plt.scatter(*zip(*testlist3))
+    plt.subplot(133)
+    lines3 = plt.scatter(*zip(*testlist4))
     plt.show()
